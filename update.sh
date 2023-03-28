61c6d17f90cb226deea41f55fc7f6db131e4331e
Anthony Tarbinian <atar137h@gmail.com>
2023-03-27 21:37:11 -0700

    updated update script with switch options

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 #
-# options for future:
-#  hostname
+# options:
 #  when: (now, later, test)
 
-sudo nixos-rebuild boot --flake .#envy-nixos --impure
+WHEN=$1
+
+if [ "$WHEN" = "later" ]; then
+	sudo nixos-rebuild boot --flake .#envy-nixos --impure
+elif [ "$WHEN" = "now" ]; then
+	sudo nixos-rebuild switch --flake .#envy-nixos --impure
+elif [ "$WHEN" = "test" ]; then
+	sudo nixos-rebuild dry-build --flake .#envy-nixos --impure
+else
+	sudo nixos-rebuild boot --flake .#envy-nixos --impure
+fi;
