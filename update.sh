7eab4966d2781ffec157e8681f682d117c2a66f9
Anthony Tarbinian <atar137h@gmail.com>
2023-12-22 12:09:25 -0800

    generalized update.sh for any hostname

--- update.sh
+++ update.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 #
 # options:
+#  host: system hostname
 #  when: (now, later, test)
+#
+# example usage:
+# ./update.sh envy now
 
-WHEN=$1
+HOST=$1
+WHEN=$2
 
 if [ "$WHEN" = "later" ]; then
-	sudo nixos-rebuild boot --flake .#envy --impure
+	sudo nixos-rebuild boot --flake .#"${HOST}" --impure
 elif [ "$WHEN" = "now" ]; then
-	sudo nixos-rebuild switch --flake .#envy --impure
+	sudo nixos-rebuild switch --flake .#"${HOST}" --impure
 elif [ "$WHEN" = "test" ]; then
-	sudo nixos-rebuild dry-build --flake .#envy --impure
+	sudo nixos-rebuild dry-build --flake .#"${HOST}" --impure
 else
-	sudo nixos-rebuild boot --flake .#envy --impure
+	sudo nixos-rebuild boot --flake .#"${HOST}" --impure
 fi;
