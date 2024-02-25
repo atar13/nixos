c656643c0256298faee0ba462d236e1b5ef5068f
Anthony Tarbinian <atar137h@gmail.com>
2024-02-24 23:35:20 -0800

    gtk3/4 dark theming

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -13,13 +13,25 @@
       name = "Tela-purple-dark";
       package = pkgs.tela-icon-theme;
     };
-    theme = {
-      name = "Materia-dark";
-      package = pkgs.materia-theme;
-    };
+    # theme = {
+    #   name = "Materia-dark";
+    #   package = pkgs.materia-theme;
+    # };
     cursorTheme = {
       name = "Bibata-Modern-Classic";
       package = pkgs.bibata-cursors;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
     };
   };
 
