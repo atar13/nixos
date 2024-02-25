82f1a6ca26dc6a63254fca075a50dd1db553e516
Anthony Tarbinian <atar137h@gmail.com>
2024-02-24 23:29:55 -0800

    more vscode extensions

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -19,6 +19,24 @@
         # sha256 = "c+sCR+Zly5KiyQdQhGG0w+e+PHWICJfKaOPFbsdL7y8=";
         #      }
         {
+          name = "remote-containers";
+          publisher = "ms-vscode-remote";
+          version = "0.345.0";
+          sha256 = "ZWFebZ6YbT0bf7M0KDtrwrrv2NJc7NUH09GTBVuzy2I=";
+        }
+        {
+          name = "marp-vscode";
+          publisher = "marp-team";
+          version = "2.8.0";
+          sha256 = "x/nR0wSOf7RXg1Sliap+qQIagcn1YD3W5OAZAcCP9ec=";
+        }
+        {
+          name = "pytrail";
+          publisher = "Samir-Rashid";
+          version = "0.0.3";
+          sha256 = "WO+OzUNSmDcAB5U9tmKIXPijTEekRkl1D0Hw68hmN6g=";
+        }
+        {
           name = "nix-env-selector";
           publisher = "arrterian";
           version = "1.0.9";
