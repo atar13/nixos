734c65228e7f5b24699f50c789f92f99dc3b784c
Anthony Tarbinian <atar137h@gmail.com>
2024-12-17 06:59:36 -0800

    xilinx modules

--- hosts/framework-16/default.nix
+++ hosts/framework-16/default.nix
@@ -1,5 +1,5 @@
 # Framework 16 Laptop specific configuration
-{ lib, config, inputs, pkgs, old-pkgs, hostname, nixos-hardware, ... }:
+{ lib, config, inputs, pkgs, old-pkgs, my-pkgs, hostname, nixos-hardware, ... }:
 {
   imports = [
     nixos-hardware.nixosModules.framework-16-7040-amd
@@ -9,14 +9,21 @@
     (import ../../modules/cli.nix { inherit inputs pkgs; })
     ../../modules/bluetooth.nix
     ../../modules/fonts.nix
-    ../../modules/gnome.nix
-    (import ../../modules/dwm.nix { inherit lib pkgs config inputs; })
-    ../../modules/desktop.nix
-    (import ../../modules/gui.nix { inherit pkgs old-pkgs; })
+    (import ../../modules/desktop.nix { inherit lib pkgs config inputs; })
+    (import ../../modules/gui.nix { inherit pkgs old-pkgs my-pkgs; })
     ../../modules/lib.nix
     ../../modules/localization.nix
     ../../modules/game.nix
   ];
+
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        # Your own overlays...
+      }
+    )
+  ] ++ [ inputs.nix-xilinx.overlay ];
+
 
   boot.initrd.kernelModules = [ "amdgpu" ];
 
@@ -57,6 +64,7 @@
       "docker"
       "libvirtd"
       "dialout"
+      "tty"
     ];
   };
 
@@ -79,13 +87,26 @@
   environment = {
     sessionVariables = {
       MOZ_USE_XINPUT2 = "1";
+      SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS = "0";
     };
     systemPackages = with pkgs; [
       old-pkgs.segger-jlink
       acpi
       virtiofsd
+      barrier
+      inputs.nix-xilinx.packages.${pkgs.system}.vitis
+      inputs.nix-xilinx.packages.${pkgs.system}.vitis_hls
+      inputs.nix-xilinx.packages.${pkgs.system}.vivado
+      inputs.nix-xilinx.packages.${pkgs.system}.model_composer
     ];
   };
+
+
+  # static ip for PYNQ board
+  # networking.interfaces.eth0.ipv4.addresses = [{
+  #   address = "192.168.2.1";
+  #   prefixLength = 24;
+  # }];
 
   boot.supportedFilesystems = [ "ntfs" ];
 
@@ -172,6 +193,34 @@
         ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", MODE="0666"
       '';
       destination = "/etc/udev/rules.d/99-ftdi.rules";
+    })
+    (pkgs.writeTextFile {
+      name = "xilinx-dilligent-usb-udev";
+      destination = "/etc/udev/rules.d/52-xilinx-digilent-usb.rules";
+      text = ''
+        ATTR{idVendor}=="1443", MODE:="666"
+        ACTION=="add", ATTR{idVendor}=="0403", ATTR{manufacturer}=="Digilent", MODE:="666"
+      '';
+    })
+    (pkgs.writeTextFile {
+      name = "xilinx-pcusb-udev";
+      destination = "/etc/udev/rules.d/52-xilinx-pcusb.rules";
+      text = ''
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0008", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0007", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0009", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="000d", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="000f", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0013", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0015", MODE="666"
+      '';
+    })
+    (pkgs.writeTextFile {
+      name = "xilinx-ftdi-usb-udev";
+      destination = "/etc/udev/rules.d/52-xilinx-ftdi-usb.rules";
+      text = ''
+        ACTION=="add", ATTR{idVendor}=="0403", ATTR{manufacturer}=="Xilinx", MODE:="666"
+      '';
     })
     old-pkgs.segger-jlink
     pkgs.saleae-logic-2
